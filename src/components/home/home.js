ec79887bb420c07dd550eafceaa18bb39923cc82
Vivek95744 <vivekdhola695@gmail.com>
2024-10-04 12:38:28 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -34,7 +34,7 @@ const [isVisible, setIsVisible] = useState(false);
       <div className="container" id="home" >
         <div className="row animate-on-scroll1">
           <p className="Text">Welcome To My world,</p>
-          <p className="Text101">Our mission revolves around</p>
+          <p className="Text101">Our</p>
           <p className="text">
             <div className="typewriter-container ">
   <Typewriter 
